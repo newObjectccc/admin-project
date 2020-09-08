66b40c67bd1b87067a7a32bd7152a05b2e53cd20
dick <897333949@qq.com>
2020-09-08 11:28:24 +0800

    cc

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import store from './store'
+import router from './router'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  store,
+  router,
+  render: h => h(App)
 }).$mount('#app')
