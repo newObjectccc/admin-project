7746b84314ebc15933311793312dc1758f0fa283
dick <897333949@qq.com>
2020-09-10 18:11:06 +0800

    cc

---
+++ src/views/layout/index.vue
@@ -0,0 +1,29 @@
+<template>
+<!-- <div :class="classObj" class="app-wrapper">
+    <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
+    <sidebar class="sidebar-container" />
+    <div :class="{hasTagsView:needTagsView}" class="main-container">
+        <div :class="{'fixed-header':fixedHeader}">
+            <navbar />
+            <tags-view v-if="needTagsView" />
+        </div>
+        <app-main />
+        <right-panel v-if="showSettings">
+            <settings />
+        </right-panel>
+    </div>
+</div> -->
+<div class="">
+
+</div>
+</template>
+
+<script>
+export default {
+    name: 'layout'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
